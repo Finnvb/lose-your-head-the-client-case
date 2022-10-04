86b4fcd5a8438b2ca7c600ce7f766cbe2de66f76
Finnvb <finnvb@gmail.com>
2022-10-04 18:43:21 +0200

    home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,28 @@
 
 </script>
 
+<main>
+<!-- <img src="/background.jpg" alt="background"> -->
+
 <slot></slot>
+
+</main>
+
+<style>
+
+    /* img{
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+    } */
+
+   main{ 
+    background-image: url("background.jpg");
+    /* height: 100vh; */
+    /* width: 100vw; */
+    background-repeat: repeat;
+    background-position: center; 
+   background-size: cover;
+
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,81 @@
-<p>hello</p>
+<script>
+</script>
+
+<main>
+	<header>
+		<h1>Stories of the <br /> ILOJO BAR</h1>
+
+		<h2>Discover the stories of the <br /> demolished bar in Lagos</h2>
+	</header>
+
+	<section>
+
+		<div class="container">
+			<img src="goldplate2.png" alt="goldplate2" />
+			<div class="centered"><a href="/">Discover Stories</a></div>
+		</div>
+
+		<img class="ilojo-bar" src="cdf-1950s.png" alt="Ilojo-bar" />
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	h1,
+	h2 {
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 4rem;
+	}
+
+	h2 {
+		font-size: 1.5rem;
+		margin: 2rem 0;
+	}
+
+	a {
+		text-decoration: none;
+		/* padding:1rem; */
+		color: white;
+		font-size: 1.5rem;
+	}
+
+	.container {
+		position: relative;
+		text-align: center;
+		color: white;
+	}
+
+	.centered {
+		position: absolute;
+		top: 0;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	img {
+		height: 6rem;
+		border-radius: 1rem;
+	}
+
+	.ilojo-bar {
+		height: 20rem;
+	}
+
+	section {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		margin: 2rem 0;
+	}
+</style>
